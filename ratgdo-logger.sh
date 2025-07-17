44a482fa0f97f8e9d7b66e15695ebb98bf0f6b54
Paul Wieland <4663918+PaulWieland@users.noreply.github.com>
2025-07-17 10:52:03 -0400

    Update ratgdo-logger.sh

--- ratgdo-logger.sh
+++ ratgdo-logger.sh
@@ -1,25 +1,41 @@
 #!/bin/bash
 
+# HOST from argument 1
 HOST="$1"
 
-# check if HOST was provided
+# Check if a host is provided
 if [ -z "$HOST" ]; then
-  echo "Usage: $0 <ratgdo IP>"
+  echo "Usage: $0 <ratgdo_hostname_or_ip>"
   exit 1
 fi
 
-STATUS_CODE=$(curl -s -o /dev/null -w "%{http_code}" "$HOST/events")
+# Check if the host is reachable
+echo "Pinging $HOST..."
+ping -c 1 -W 2 "$HOST" > /dev/null 2>&1
 
-if [ "$STATUS_CODE" -eq 200 ]; then
-  echo "✅ $HOST is reachable (200 OK), esphome detected"
-  exit 0
+if [ $? -ne 0 ]; then
+  echo "❌ Host $HOST is unreachable."
+  exit 1
 else
-  echo "❌ $HOST returned HTTP status code $STATUS_CODE"
+  echo "✅ Host $HOST is reachable."
+fi
+
+# Check if http://HOST/events returns HTTP 200 (ESPHome detected)
+URL="http://$HOST/events"
+echo "Checking firmware type..."
+
+STATUS_CODE=$(curl --head -s -o /dev/null -w "%{http_code}" "$URL")
+
+# TO DO: add case for homekit firmware
+if [ "$STATUS_CODE" -eq 200 ]; then
+  echo "✅ ratgdo-esphome detected"
+else
+  echo "❌ unknown firmware type"
   exit 1
 fi
 
-
-curl -s --no-buffer $HOST/events | while IFS= read -r line; do
+# Begin log capture
+curl -s --no-buffer $URL | while IFS= read -r line; do
   clean_line=$(printf "%s" "$line" | tr -d '\r')
   if [[ "$clean_line" == "event: log" ]]; then
     read -r next_line
